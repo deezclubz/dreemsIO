ef8e966d68d9fa20592d5b9d5b6e160035f1dc57
Timofey <pttima@yandex.ru>
2025-04-30 18:46:00 +0300

    added real-time collab

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import './App.css'
 import { Tldraw } from 'tldraw'
+import { useSyncDemo } from '@tldraw/sync'
 import 'tldraw/tldraw.css'
 
-const App =()=> {
-
-  return (
-    <>
-     <h1>yo!</h1>
-     <div style={{ position: 'fixed', inset: 0 }}>
-			  <Tldraw />
-		</div>
-    </>
-  )
+const App = () => {
+	const store = useSyncDemo({ roomId: 'myapp-abc123' })
+	return (
+		<>
+			<h1>yo!</h1>
+			<div style={{ position: 'fixed', inset: 0 }}>
+				<Tldraw store={store} />
+			</div>
+		</>
+	)
 }
 
 export default App
