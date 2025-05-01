90e3ea2feaf490922c273b12650000f8815612f8
Eugeny Gorshkov <gorshkov.zhenya@mail.ru>
2025-05-01 17:31:11 +0300

    feat: add connect to custom server

--- src/components/tildraw.tsx
+++ src/components/tildraw.tsx
@@ -1,14 +1,26 @@
-import { useSyncDemo } from '@tldraw/sync'
-import { Tldraw } from 'tldraw'
+import { RemoteTLStoreWithStatus } from '@tldraw/sync'
+import { TLBookmarkAsset, Tldraw } from 'tldraw'
 
-export const Tildraw = () => {
-	const store = useSyncDemo({ roomId: 'dreemsIO' })
+interface TildrawProps {
+	store: RemoteTLStoreWithStatus
+	unfurlBookmarkUrl: (args: { url: string }) => Promise<TLBookmarkAsset>
+}
+export const Tildraw:React.FC<TildrawProps> = (props) => {
+	const { store,unfurlBookmarkUrl } = props
 	return (
 		<div>
 			<div
 				style={{ position: 'fixed', inset: 0, width: '50vw', height: '50vh' }}
 			>
-				<Tldraw store={store} />
+				<Tldraw 
+					store={store} 
+					onMount={(editor) => {
+						// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+						// @ts-expect-error
+						window.editor = editor
+						// when the editor is ready, we need to register out bookmark unfurling service
+						editor.registerExternalAssetHandler('url', unfurlBookmarkUrl)
+				}}/>
 			</div>
 		</div>
 	)
